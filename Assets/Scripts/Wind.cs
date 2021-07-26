faecaa4c036b12b0a5201bdbf0097bbbc7f4f5d0
박상빈 <jixg20@gmail.com>
2021-07-26 14:16:52 +0900

    포탈

---
+++ Assets/Scripts/Wind.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Wind : MonoBehaviour
+{
+    private float radian,x,y , rotateDegree;
+
+    private Rigidbody2D objectRigidbody2D;
+    private void Start(){
+        rotateDegree = transform.rotation.z;
+        radian = rotateDegree*Mathf.PI/180f;
+        x = Mathf.Cos(radian);
+        y = Mathf.Sin(radian);
+    }
+    
+    
+    private void OnTriggerStay2D(Collider2D collision){
+        if(collision.gameObject.transform.parent == null){
+           objectRigidbody2D = collision.gameObject.transform.GetComponent<Rigidbody2D>();
+        }
+        else{
+            objectRigidbody2D = collision.gameObject.transform.parent.GetComponent<Rigidbody2D>();
+        }
+       objectRigidbody2D.velocity = new Vector2(objectRigidbody2D.velocity.x * x * 3f, objectRigidbody2D.velocity.y * y* 100f);
+    }
+}
