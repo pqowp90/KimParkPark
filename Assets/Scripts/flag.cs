7ecc8dd0d7bc516c7fcf10e0a05e183c0258dee6
DESKTOP-OHD6DA9\USER <zelodoc08@gmail.com>
2021-07-26 15:58:29 +0900

    깃발고침

--- Assets/Scripts/flag.cs
+++ Assets/Scripts/flag.cs
@@ -6,7 +6,7 @@ public class flag : MonoBehaviour
 {
     private float rotateDegree;
     private Rigidbody2D myRigidbody2D;
-    public bool isLook=false;
+    public bool isLook=false,isFloor;
     void Start()
     {
         myRigidbody2D = GetComponent<Rigidbody2D>();
@@ -18,8 +18,11 @@ public class flag : MonoBehaviour
             FlagRotation();
         }
     }
+    void OnCollisionEnter2D(Collision2D collision){
+        isLook = false;
+        isFloor = true;
+    }
     private void FlagRotation(){
-        
         rotateDegree = Mathf.Atan2(myRigidbody2D.velocity.y, myRigidbody2D.velocity.x);
         transform.localEulerAngles = new Vector3(0, 0, (rotateDegree * 180) / Mathf.PI);
     }
