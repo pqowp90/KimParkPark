b9eef93c7f13970aa9e68ac5748d50f9435cf0c4
suan9340 <sue9340@gmail.com>
2021-07-27 10:03:38 +0900

    Revert "z"
    This reverts commit 51c8e5acda435c9d4474c0d239c21860930046b1.

--- Assets/Scripts/PlayerMove.cs
+++ Assets/Scripts/PlayerMove.cs
@@ -21,7 +21,7 @@ public class PlayerMove : MonoBehaviour
     public static GameObject _player;
     public float fallDistance = 20;
     public float maxPosition = 0;
-    public bool isDamaged = false;
+    private bool isDamaged = false;
     
     private PlayerHPUI playerHpUI;
     // Start is called before the first frame update
--- Assets/Scripts/PlayerSpawn.cs
+++ Assets/Scripts/PlayerSpawn.cs
@@ -4,31 +4,18 @@ using UnityEngine;
 
 public class PlayerSpawn : MonoBehaviour
 {
-    // 깃발 위치 받아오기
     public Transform spawnPoint;
-    public static bool isTeleportation = false;
-    private PlayerMove playerMove;
     void Start()
     {
-        playerMove = FindObjectOfType<PlayerMove>();
+
     }
 
     void Update()
     {
-        if(playerMove.isDamaged)
-        {
-            Teleportaion();
-        }
-    }
-    public void Teleportaion()
-    {
-        if (isTeleportation) return;
-        isTeleportation = true;
-        Go();
+        
     }
     private void Go()
     {
-        transform.position = spawnPoint.position;
-        //isTeleportation = false;
+        spawnPoint.position = transform.position;
     }
 }
