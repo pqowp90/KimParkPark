e6bd21e7933df7dd82a6d64e3298470618372173
DESKTOP-OHD6DA9\USER <zelodoc08@gmail.com>
2021-07-28 01:44:44 +0900

    ㅇ

--- Assets/Scripts/Device/Barrier.cs
+++ Assets/Scripts/Device/Barrier.cs
@@ -6,8 +6,8 @@ public class Barrier : MonoBehaviour , IActive
 {
     private void OnTriggerStay2D(Collider2D collision){
         Debug.Log("dd");
-        collision.gameObject.GetComponent<Rigidbody2D>().velocity = Vector2.zero;
-        collision.transform.position -= (transform.position - collision.transform.localPosition)*0.4f;
+        collision.gameObject.GetComponent<Rigidbody2D>().velocity = -transform.position + collision.transform.position;
+        //collision.transform.position -= (transform.position - collision.transform.localPosition)*0.4f;
         collision.GetComponent<flag>().isLook = false;
         if(!collision.gameObject.GetComponentInParent<PlayerMove>())return;
         collision.gameObject.GetComponentInParent<PlayerMove>().Throwing();
