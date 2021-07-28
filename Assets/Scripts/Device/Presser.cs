555d75e4908536dfceac02081ecaf27584c38d0f
DESKTOP-OHD6DA9\USER <zelodoc08@gmail.com>
2021-07-28 10:28:56 +0900

    발판

--- Assets/Scripts/Device/Presser.cs
+++ Assets/Scripts/Device/Presser.cs
@@ -4,19 +4,43 @@ using UnityEngine;
 
 public class Presser : MonoBehaviour
 {
-    [SerializeField]
-    private GameObject things;
+    private Pipe pipe;
 
     private IActive active;
+    [SerializeField]
+    private AudioSource audioSource;
+    private Vector3 startPos;
+    private bool on;
+    [SerializeField]
+    private Transform hihi;
 
+    private void Update(){
+        
+    }
     private void Start(){
-        active = things.GetComponent<IActive>();
+        startPos = hihi.transform.position;
+        pipe = GetComponentInChildren<Pipe>();
+        audioSource = GetComponent<AudioSource>();
     }
     private void OnTriggerEnter2D(Collider2D collision){
-        active.Active(true); // on
+        if((collision.gameObject.layer == 9||collision.gameObject.layer == 2)&&!on){
+            on = true;
+            hihi.transform.position = startPos+Vector3.down*0.1f;
+            MusicSound();
+            audioSource.Play();
+            pipe.on = on;
+        }
     }
     private void OnTriggerExit2D(Collider2D collision){
-        active.Active(false); // off
+        if((collision.gameObject.layer == 9||collision.gameObject.layer == 2)&&on){
+            on = false;
+            audioSource.Play();
+            hihi.transform.position = startPos;
+            pipe.on = on;
+        }
+    }
+    private void MusicSound(){
+        audioSource.volume = PlayerPrefs.GetFloat("Volume",1f);
     }
 
 }
